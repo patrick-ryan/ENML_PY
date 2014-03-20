3793b88b06e45630d3291c19adfeb32de5642096
Patrick Ryan <pryan@virginia.edu>
2014-03-20 11:45:53 -0400

    Added Flask integration and HTMLToENML.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,55 @@
 # -*- coding: utf-8 -*- 
 import os
 from bs4 import BeautifulSoup
+from flask import url_for
+
 MIME_TO_EXTESION_MAPPING = {
     'image/png': '.png',
     'image/jpg': '.jpg',
+    'image/jpeg': '.jpg',
     'image/gif': '.gif'
 }
+
+PROHIBITED_ENML_ELEMENTS = [
+    "applet", "base", "basefont", "bgsound", "blink", "body", "button", "dir", "embed", "fieldset", 
+    "form", "frame", "frameset", "head", "html", "iframe", "ilayer", "input", "isindex", "label", 
+    "layer", "legend", "link", "marquee", "menu", "meta", "noframes", "noscript", "object", "optgroup", 
+    "option", "param", "plaintext", "script", "select", "style", "textarea", "xml", "id", "class", 
+    "onclick", "ondblclick", "on", "accesskey", "data", "dynsrc", "tabindex"
+]
+
+VALID_URL_PROTOCOLS = [
+    "http", "https", "file"
+]
+
+def HTMLToENML(content):
+    """
+    converts HTML string into ENML string
+    """
+    soup = BeautifulSoup(content)
+
+    todos = soup.find_all(type="checkbox")
+    for todo in todos:
+        checkbox = soup.new_tag('en-todo')
+        if todo.has_attr('checked'):
+            checkbox['checked'] = todo['checked']
+        todo.replace_with(checkbox)
+
+    images = soup.find_all('img')
+    for image in images:
+        new_tag = soup.new_tag('en-media')
+        hash_str, type_ext = image['src'].rsplit("/",1)[1].rsplit(".",1)
+        new_tag['hash'] = hash_str
+        new_tag['type'] = "image/" + type_ext
+        if image.has_attr('alt'):
+            new_tag['alt'] = image['alt']
+        image.replace_with(new_tag)
+
+    body = soup.find('body')
+    body.name = 'en-note'
+    [tag.extract() for tag in soup(PROHIBITED_ENML_ELEMENTS)]
+    note = str(body)
+    return note
 
 def ENMLToHTML(content, pretty=True, **kwargs):
     """
@@ -31,15 +75,17 @@ def ENMLToHTML(content, pretty=True, **kwargs):
             # TODO: use different tags for different mime-types
             new_tag = soup.new_tag('img')
             new_tag['src'] = resource_url
+            if media.has_attr('alt'):
+                new_tag['alt'] = media['alt']
             media.replace_with(new_tag)
     
     note = soup.find('en-note')
-    body = soup.new_tag('body')
     html = soup.new_tag('html')
     html.append(note)
     note.name = 'body'
 
-    output = html.prettify().encode('utf-8') if pretty else str(html)
+    # output = html.prettify().encode('utf-8') if pretty else str(html)
+    output = str(html)
     return output
 
 
@@ -63,26 +109,26 @@ class MediaStore(object):
     def save(self, hash_str, mime_type):
         pass
 
+# flask integration added
 class FileMediaStore(MediaStore):
     def __init__(self, note_store, note_guid, path):
         """
         note_store: NoteStore object from EvernoteSDK
         note_guid: Guid of the note in which the resouces exist
-        path: The path to store media file
         """
         super(FileMediaStore, self).__init__(note_store, note_guid)
-        self.path = os.path.abspath(path)
+        self.path = path
     
     def save(self, hash_str, mime_type):
         """
         save the specified hash and return the saved file's URL
         """
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
         data = self._get_resource_by_hash(hash_str)
-        file_path = self.path + '/'  + hash_str + MIME_TO_EXTESION_MAPPING[mime_type]
+        filename = self.path + '/' + hash_str + MIME_TO_EXTESION_MAPPING[mime_type]
+        file_path = os.path.abspath('static') + '/' + filename
         f = open(file_path, "w")
         f.write(data)
         f.close()
-        return "file://" + file_path
+        flask_path = url_for('static', filename=filename)
+        return flask_path
         
